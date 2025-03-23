74ee40ad8bcad1f96b36582f1854f88b175c7b51
Altair-Bueno <altair.bueno@uma.es>
2025-03-23 21:22:13 +0100

    tests: Include tests for alloc encoders

--- src/impls/alloc.rs
+++ src/impls/alloc.rs
@@ -41,3 +41,32 @@ impl<E: Encoder> Encodable<E> for String {
         encoder.put_slice(self.as_bytes())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn assert_that_vecs_can_be_encoded() {
+        let encodable = vec![1, 2, 3, 4, 5];
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_boxes_can_be_encoded() {
+        let encodable = Box::new(5u8);
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_cows_can_be_encoded() {
+        let encodable = Cow::Borrowed("hello");
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_strings_can_be_encoded() {
+        let encodable = String::from("hello");
+        encodable.encode(&mut ()).unwrap();
+    }
+}
