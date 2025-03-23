1416ee2e45d901173714c2f14ceab81a5b39c94a
Altair-Bueno <altair.bueno@uma.es>
2025-03-23 21:37:09 +0100

    tests: Add tests for slice encoders and CStr size verification

--- src/impls/slices.rs
+++ src/impls/slices.rs
@@ -50,3 +50,38 @@ where
         encoder.put_slice(self.to_bytes_with_nul())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::EncodableSize as _;
+
+    #[test]
+    fn assert_that_arrays_can_be_encoded() {
+        let encodable = [1, 2, 3, 4, 5];
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_slices_can_be_encoded() {
+        let encodable = [1, 2, 3, 4, 5].as_slice();
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_strs_can_be_encoded() {
+        let encodable = "Hello, world!";
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_cstrs_can_be_encoded() {
+        let encodable = c"Hello, world!";
+        encodable.encode(&mut ()).unwrap();
+        assert_eq!(
+            encodable.encoded_size().unwrap(),
+            encodable.count_bytes() + 1,
+            "CStr must include the null terminator"
+        )
+    }
+}
