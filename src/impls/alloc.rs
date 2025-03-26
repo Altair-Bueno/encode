1f0c9b5771175482b4eef623efe5194e6b8206ed
Altair Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2025-03-26 20:10:39 +0100

    feat: Add support for CString (#12)

--- src/impls/alloc.rs
+++ src/impls/alloc.rs
@@ -1,13 +1,13 @@
 use crate::Encodable;
 use crate::Encoder;
-use alloc::{borrow::Cow, borrow::ToOwned, boxed::Box, string::String, vec::Vec};
+use alloc::{borrow::Cow, borrow::ToOwned, boxed::Box, ffi::CString, string::String, vec::Vec};
 
 impl<E: Encoder> Encodable<E> for Vec<u8> {
     type Error = E::Error;
 
     #[inline]
     fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
-        encoder.put_slice(self.as_slice())
+        self.as_slice().encode(encoder)
     }
 }
 
@@ -38,20 +38,46 @@ impl<E: Encoder> Encodable<E> for String {
 
     #[inline]
     fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
-        encoder.put_slice(self.as_bytes())
+        self.as_str().encode(encoder)
+    }
+}
+
+impl<E: Encoder> Encodable<E> for CString {
+    type Error = E::Error;
+
+    #[inline]
+    fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
+        self.as_c_str().encode(encoder)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use alloc::borrow::Cow;
-
+    use super::*;
     use crate::Encodable;
 
     #[test]
-    fn assert_that_cow_slice_can_be_encoded() {
-        let cow: Cow<'_, [u8]> = Cow::Borrowed(&[][..]);
+    fn assert_that_boxes_can_be_encoded() {
+        let encodable = Box::new(5u8);
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_cows_can_be_encoded() {
+        let cow = Cow::Borrowed("hello");
         // Explicit fully qualified call because otherwise autoref could just encode `&[u8]`.
         <Cow<'_, _> as Encodable<()>>::encode(&cow, &mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_strings_can_be_encoded() {
+        let encodable = String::from("hello");
+        encodable.encode(&mut ()).unwrap();
+    }
+
+    #[test]
+    fn assert_that_cstrings_can_be_encoded() {
+        let encodable = CString::from(c"hello");
+        encodable.encode(&mut ()).unwrap();
     }
 }
