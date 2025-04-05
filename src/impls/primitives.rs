b8334f2dc825f50a61c974c90118813c21253284
Altair-Bueno <altair.bueno@uma.es>
2025-04-05 17:56:27 +0200

    tests: Refactor encoding tests to use buffer and assert results

--- src/impls/primitives.rs
+++ src/impls/primitives.rs
@@ -59,33 +59,90 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+
+    const BUF_SIZE: usize = 64;
+
     #[test]
     fn assert_that_chars_can_be_encoded() {
+        let expected = b"a";
         let encodable = 'a';
-        encodable.encode(&mut ()).unwrap();
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
-    fn assert_that_bytes_can_be_encoded() {
-        let encodable = 42u8;
-        encodable.encode(&mut ()).unwrap();
+    fn assert_that_unsigned_bytes_can_be_encoded() {
+        let expected = b"\xFF";
+        let encodable = u8::MAX;
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
     fn assert_that_signed_bytes_can_be_encoded() {
-        let encodable = 42i8;
-        encodable.encode(&mut ()).unwrap();
+        let expected = b"\x80";
+        let encodable = i8::MIN;
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
-    fn assert_that_bools_can_be_encoded() {
+    fn assert_that_true_can_be_encoded() {
+        let expected = b"\x01";
         let encodable = true;
-        encodable.encode(&mut ()).unwrap();
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn assert_that_false_can_be_encoded() {
+        let expected = b"\x00";
+        let encodable = false;
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
     fn assert_that_references_to_encodables_can_be_encoded() {
-        let encodable = &42u8;
-        encodable.encode(&mut ()).unwrap();
+        let expected = b"\x00";
+        let encodable = &false;
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 }
