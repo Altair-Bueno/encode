5f4ce9c2947237e3d9650b2c92371bbfc22873a9
Altair-Bueno <altair.bueno@uma.es>
2025-04-05 17:49:55 +0200

    ref/tests: Refactor encoding logic and update tests
    Refactored encoding logic to use `.encode` method directly. Updated tests to verify encoded output against expected results.

--- src/impls/slices.rs
+++ src/impls/slices.rs
@@ -23,7 +23,7 @@ where
 
     #[inline]
     fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
-        encoder.put_slice(self)
+        (self as &[u8]).encode(encoder)
     }
 }
 
@@ -35,7 +35,7 @@ where
 
     #[inline]
     fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
-        encoder.put_slice(self.as_bytes())
+        self.as_bytes().encode(encoder)
     }
 }
 
@@ -47,41 +47,69 @@ where
 
     #[inline]
     fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
-        encoder.put_slice(self.to_bytes_with_nul())
+        self.to_bytes_with_nul().encode(encoder)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::EncodableSize as _;
+
+    const BUF_SIZE: usize = 64;
 
     #[test]
     fn assert_that_arrays_can_be_encoded() {
-        let encodable = [1, 2, 3, 4, 5];
-        encodable.encode(&mut ()).unwrap();
+        let expected = b"\x01\x02\x03";
+        let encodable: [u8; 3] = [1, 2, 3];
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
     fn assert_that_slices_can_be_encoded() {
-        let encodable = [1, 2, 3, 4, 5].as_slice();
-        encodable.encode(&mut ()).unwrap();
+        let expected = b"\x01\x02\x03";
+        let encodable: &[u8] = &[1, 2, 3];
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
     fn assert_that_strs_can_be_encoded() {
-        let encodable = "Hello, world!";
-        encodable.encode(&mut ()).unwrap();
+        let expected = b"Hello world!";
+        let encodable = "Hello world!";
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 
     #[test]
     fn assert_that_cstrs_can_be_encoded() {
-        let encodable = c"Hello, world!";
-        encodable.encode(&mut ()).unwrap();
-        assert_eq!(
-            encodable.encoded_size().unwrap(),
-            encodable.count_bytes() + 1,
-            "CStr must include the null terminator"
-        )
+        let expected = b"Hello world!\0";
+        let encodable = c"Hello world!";
+
+        let mut buf = [0u8; BUF_SIZE];
+        let mut encoder = &mut buf as &mut [u8];
+        encodable.encode(&mut encoder).unwrap();
+        let written = BUF_SIZE - encoder.len();
+        let result = &buf[..written];
+
+        assert_eq!(expected, result);
     }
 }
