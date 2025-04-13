217d208cd7c4bb2c9835011c5b4c6723c9126e07
Altair-Bueno <altair.bueno@uma.es>
2025-04-13 19:46:59 +0200

    tests: Add tests for Formatter

--- src/encoders/fmt.rs
+++ src/encoders/fmt.rs
@@ -13,3 +13,35 @@ impl StrEncoder for Formatter<'_> {
         self.write_str(string)
     }
 }
+
+#[cfg(all(test, feature = "alloc"))]
+mod tests {
+    use super::*;
+    use crate::Encodable;
+    use core::fmt::Write;
+
+    #[test]
+    fn assert_that_formatter_can_be_used_as_an_encoder() {
+        struct MyEncodable;
+
+        impl<E> Encodable<E> for MyEncodable
+        where
+            E: StrEncoder,
+        {
+            type Error = E::Error;
+            fn encode(&self, encoder: &mut E) -> Result<(), Self::Error> {
+                "Hello world".encode(encoder)
+            }
+        }
+
+        impl core::fmt::Display for MyEncodable {
+            fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), core::fmt::Error> {
+                self.encode(f)
+            }
+        }
+        let mut output = String::new();
+        writeln!(&mut output, "{}", MyEncodable).expect("Failed to write to output");
+
+        assert_eq!(output, "Hello world\n");
+    }
+}
